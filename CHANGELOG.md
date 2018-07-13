4aecfd4069d02aeec4cf2a0dab8d16a48a29e3ef
Jakub Biesiada <biesiadajakub@icloud.com>
2018-07-13 15:37:38 +0200

    v1.0.2

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,14 @@
 # react-universal-tilt Changelog
 
+## 1.0.1 / 1.0.2 (2018-07-13)
+#### Bug Fix
+- the incompatibility problem has been fixed
+- previous versions are deprecated
+
+#### Repository Changes
+- added Webpack & Babel v7
+- changed default file to minified
+
 ## 1.0.0 beta 1 / 1.0.0 (2018-06-18)
 #### New Feature
 - added ReactTilt component with `options`, `style` and `className` options
---
+++ webpack.config.js
@@ -0,0 +1,57 @@
+const webpack = require('webpack');
+const path = require('path');
+const env = require('yargs').argv.env;
+
+let libraryName = 'react-universal-tilt';
+
+let outputFile, mode;
+
+if (env === 'build') {
+  mode = 'production';
+  outputFile = libraryName + '.min.js';
+} else {
+  mode = 'development';
+  outputFile = libraryName + '.js';
+}
+
+const config = {
+  mode: mode,
+  entry: __dirname + '/src/index.js',
+  externals: {
+    react: {
+      root: 'React',
+      commonjs2: 'react',
+      commonjs: 'react',
+      amd: 'react'
+    },
+    'universal-tilt.js': {
+      root: 'UniversalTilt',
+      commonjs2: 'universal-tilt.js',
+      commonjs: 'universal-tilt.js',
+      amd: 'universal-tilt.js'
+    }
+  },
+  devtool: 'source-map',
+  output: {
+    path: __dirname + '/lib',
+    filename: outputFile,
+    library: 'ReactTilt',
+    libraryTarget: 'umd',
+    umdNamedDefine: true
+  },
+  module: {
+    rules: [
+      {
+        test: /(\.jsx|\.js)$/,
+        loader: 'babel-loader',
+        exclude: /(node_modules|bower_components)/
+      }
+    ]
+  },
+  resolve: {
+    modules: [path.resolve('./node_modules'), path.resolve('./src')],
+    extensions: ['.json', '.js']
+  }
+};
+
+module.exports = config;
