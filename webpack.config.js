0d6d0f1a4b28a3ad819f8d505e4dde91d58df4c9
Jakub Biesiada <biesiadajakub@icloud.com>
2018-12-24 09:54:59 +0100

    Update

--- webpack.config.js
+++ webpack.config.js
@@ -44,7 +44,7 @@ const config = {
     rules: [
       {
         test: /\.js$/,
-        exclude: /(node_modules|bower_components)/,
+        exclude: /node_modules/,
         use: {
           loader: 'babel-loader'
         }
