b5e326e251d4976c824ca5ac24c69520c4d2a926
Jakub Biesiada <biesiadajakub@icloud.com>
2019-08-30 00:21:34 +0200

    Update

--- stories/index.stories.js
+++ stories/index.stories.js
@@ -6,7 +6,7 @@ import { withKnobs, text, boolean, number } from '@storybook/addon-knobs';
 
 import ReactTilt from '../src';
 
-const stories = storiesOf('ReactUniversalTilt');
+const stories = storiesOf('ReactUniversalTilt', module);
 
 stories.addDecorator(withKnobs);
 
